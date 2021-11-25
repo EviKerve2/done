653b9522d044167c83664ff2ca0e5e8d8df55cea
opilane <opilane@HP-CZC6297B82>
2021-11-25 09:46:01 +0200

    fxdghshsh

--- done.py
+++ done.py
@@ -1,36 +1,41 @@
-﻿from Funktsioonid import*
-user=[Evi]
-psword=[12345]
-
+﻿from Funktsioonid import *
+users=loe_failist_listisse("users.txt")
+passwords=loe_failist_listisse("passwords.txt")
+print(users)
+print(passwword)
 while True:
-    print("Näita kõike-0, Reg-1,  ")
-    v=int(input())
-    if v==0:
-        koik_kasu
-    elif v==1:
-        print("Registreerimine")
-        while 1:
-            log=input("Kasutajatunnus")
-
-
-
-
-
-
-
-
-        elif v.upper()=="I":
-            while 1:
-                pas=input("Sisesta oma parool")
-                tulemus=paskontroll(pas)
-                if tulemus==True:
-                    users.append(log)
-                    password.append(pas)
-                    break
-    elif v==2:
-        print("Avtoriseerimine")
-        if passwords.index(pas)==users.index(user):
-            print("Tere tulemast")
-
-    elif v==3:
-        print("Välja")
+	print("Näita kõike -0,Reg-1,Avt-2,Välja-3")
+	v=int(input())
+	if v==0:
+		koik_kasutajad(users,passwords)
+	elif v==1:
+		print("Registreerimine")
+		while 1:
+			log=input("Kasutajatunnus")
+			if log not in users:
+				print("Tunnus soobib")
+				break
+			else:
+				print("See nimi juba on olemas listis")	
+		v=input("Arvuti-A või ise-I loob parool")
+		if v.upper()=="A":
+			pas=passautomat()
+		elif v.upper()=="I":			
+			while 1:
+				pas=input("Sisesta oma parool")
+				tulemus=paskontroll(pas)
+				if tulemus==True:
+					users.append(log)
+					passwords.append(pas)
+					break		
+	elif v==2:
+		print("Avtoriseerimine")
+		if passwords.index(pas)==users.index(user):
+			print("Tere tulemast")
+		
+	elif v==3:
+		print("Välja")
+		break
+		#valmis
+	else:
+		print("On vaja valida 1,2 või 3")# kõik on olemas
